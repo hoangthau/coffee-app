1c76dbf4a68abcca497f8e73193504988425185e
Thau Nguyen <nghoangthau@gmail.com>
2023-03-26 20:46:22 +0700

    feat: #10 log props

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ import useTrackLocation from '@/hooks/use-track-location';
 import { useStore, ACTION_TYPES } from '@/store/store-context';
 
 export default function Home(props) {
-  console.log('test 456');
+  console.log('test');
   const { handleTrackLocation, latLong } = useTrackLocation();
   const [isLoading, setLoading] = useState(false);
 
