884d5f95a7aca98199b81a28ad8d4ab4a04ffb6c
Thau Nguyen <nghoangthau@gmail.com>
2023-03-26 18:54:00 +0700

    feat: #9 update log ab

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,7 +1,7 @@
 import { table, getMinifiedRecords, findRecordByFilter } from '../../lib/airtable';
 
 const createCoffeeStore = async (req, res) => {
-  console.log('a', req);
+  console.log('ab', req);
   if (req.method === 'POST') {
     //find a record
 
